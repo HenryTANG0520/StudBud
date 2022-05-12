9cb47f703555b837bc1533903297a5ea1b3dfa8a
HenryTANG0520 <Henrytorres0520@outlook.com>
2022-05-12 22:44:13 +1000

    git push

--- html/trigger.js
+++
@@ -1,9 +0,0 @@
-var isFHD = screen.width > 1024 ? ture : false;
-alert(screen.width + ":" + screen.height);
-(function(){
-    if (isFHD) {
-        window.location = "index_pc.html";
-    }else{
-        window.location = "index_mb.html";
-    }
-})
