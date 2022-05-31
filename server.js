b2bda4ebf021a6e106abb8375856fe858688c1fb
HenryTANG0520 <Henrytorres0520@outlook.com>
2022-05-31 21:23:59 +1000

    update-parcel dist update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static(_dirname + '/dist'));
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.html')
+    res.sendFile(_dirname + '/dist/index.html')
 });
 let server = app.listen(8888, function () {
     console.log("App server is running on port 8888");
