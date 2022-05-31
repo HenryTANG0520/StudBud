8b6bb6ae5c0e5565a8bee268166a05512c08b0f8
HenryTANG0520 <Henrytorres0520@outlook.com>
2022-05-31 22:58:45 +1000

    update

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(_dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 app.get('/', function (req, res) {
-    res.sendFile(_dirname + '/dist/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
 });
 let server = app.listen(8888, function () {
     console.log("App server is running on port 8888");
