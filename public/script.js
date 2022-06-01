8dab5e1c78d296b993725ce078f39b8644c5eb5d
HenryTANG0520 <Henrytorres0520@outlook.com>
2022-06-01 14:24:13 +1000

    update-navibar selector update (bugs include)

--- public/script.js
+++ public/script.js
@@ -3,3 +3,26 @@ import "./js/flowTimeTracker";
 import "./js/kanban";
 import "./js/dictionary";
 import "./js/music";
+
+window.onload = function(){
+    var naviList = document.getElementsById("fList");
+    var aList = naviList.getElementsByTagName("a");
+    var cDiv = document.getElementById("cDiv");
+    var contents = cDiv.getElementsByTagName("div");
+
+    for(var i = 0; i < aList.length; i++){
+        aList[i].index = i;
+        aList[i].onclick = function(){
+            for(var j = 0; j < aList.length; j++){
+                aList[j].className = '';
+            }
+            this.className = 'activate';
+
+            for(var j = 0; j < contents.length; j++){
+                contents[j].className = 'function contentHide';
+            }
+
+            contents[this.index].className = "function contentShow";
+        }
+    }
+}
